7a73341ca2f37eb2dc5e82355ef8b72801d0bcaa
saniyar-dev <saniyar.dev@gmail.com>
2022-08-21 12:24:31 +0430

    finished StyledComponent setup and test

---
+++ src/components/Button.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const Button = styled.button`
+/* Rectangle 11 */
+
+
+position: absolute;
+width: 142px;
+height: 60px;
+left: 916px;
+top: 220px;
+
+background: linear-gradient(262.49deg, #05F4F1 -27.69%, #00FF87 112.29%);
+border-radius: 8px;
+`
