89a690583af4bf9ae59988df6ed69447ff1acaf8
saniyar-dev <saniyar.dev@gmail.com>
2022-08-22 18:22:30 +0430

    fixed Button component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,24 +1,6 @@
 import React, {FC, ReactElement} from "react";
 import styled from "styled-components";
-
-const Button = styled.button<{
-    width: number;
-    height: number;
-    bgColor: string;
-    active: boolean;
-}>`
-    width: ${props => props.width + 'px'};
-    height: ${props => props.height + 'px'};
-    background: ${props => {
-        return props.active ? props.bgColor : 'white'
-    }};
-    font-family: 'Vazirmatn';
-    font-style: normal;
-    font-weight: 900;
-    font-size: 17px;
-    text-align: center;
-    border-radius: 8px;
-`
+import Button from "../Button/Button";
 
 const Container = styled.div`
     background: #FFFFFF;
