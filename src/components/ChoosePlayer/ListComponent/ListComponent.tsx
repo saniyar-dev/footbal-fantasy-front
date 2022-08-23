c75e8222d6e3d3330084173c1626564d39bb164b
saniyar-dev <saniyar.dev@gmail.com>
2022-08-23 17:48:49 +0430

    setup of ListHeader component

--- src/components/ChoosePlayer/ListComponent/ListComponent.tsx
+++ src/components/ChoosePlayer/ListComponent/ListComponent.tsx
@@ -77,9 +77,39 @@ const Container = styled.div`
     justify-content: start;
     align-items: center;
 `
+
+const ListHeader = styled.div`
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+`
+
+const Colomn = styled.span`
+    font-family: 'Vazirmatn';
+    font-style: normal;
+    font-weight: 300;
+    font-size: 12px;
+    display: flex;
+    align-items: center;
+    text-align: right;
+
+    color: #999999;
+`
+
 const ListComponent: FC = (): ReactElement => {
     return (
         <Container>
+            <ListHeader>
+                <Colomn>
+                    نام بازیکن
+                </Colomn>
+                <Colomn>
+                   عملکرد 
+                </Colomn>
+                <Colomn>
+                    قیمت
+                </Colomn>
+            </ListHeader>
             {
                 ListItems.map(listItem => {
                     return <ListItem width={240} height={40} name={listItem.name} position={'CEH'}
