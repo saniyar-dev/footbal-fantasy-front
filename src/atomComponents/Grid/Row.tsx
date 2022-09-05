34de79547d37b174b468462f71e8c371a2af2ff5
saniyar.dev <saniyar.dev@gmail.com>
2022-09-05 11:12:11 +0000

    Resolve "grid components"

---
+++ src/atomComponents/Grid/Row.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+import {CSSProperties} from "react";
+
+type Style = Pick<CSSProperties, "gap" | "width" | "height">
+
+const Row = styled.div<{
+    styles: Style
+}>`
+    display: flex;
+    width: ${props => props.styles.width};
+    height: ${props => props.styles.height};
+    gap: ${props => props.styles.gap};
+`
+export default Row
+
