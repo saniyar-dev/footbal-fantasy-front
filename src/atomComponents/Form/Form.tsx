1dd71fab202ceb66f63de03e28dbe1caf0d13c05
saniyar-dev <saniyar.dev@gmail.com>
2022-09-06 18:56:13 +0430

    stage-02

--- src/atomComponents/Form/Form.tsx
+++ src/atomComponents/Form/Form.tsx
@@ -1,5 +1,7 @@
 import React, {createContext, FC, ReactElement, ReactNode} from 'react'
-import styled from "styled-components";
+import styled, { CSSProperties } from "styled-components";
+import Container from '../Grid/Container';
+import Row from '../Grid/Row';
 
 const FormContext = createContext<{
     onSubmitFn: () => void,
@@ -9,8 +11,22 @@ const FormContext = createContext<{
     onSubmitFn: () => {throw new Error("form doesn't have onSubmitFn function")}
 })
 
+const StyledForm = styled(Container)`
+
+`
 export const Form: FC<{
-    children: ReactNode
-}> = ({children}): ReactElement => {
-    return <div>{children}</div>
+    children: ReactNode,
+    styles?: Pick<CSSProperties, "gap" | "gridTemplateColumns" | "gridTemplateRows" | "width" | "height">,
+}> = ({children, styles}): ReactElement => {
+    return <StyledForm styles={styles}>{children}</StyledForm>
+}
+
+export const FormHeader: FC<{
+    children: ReactNode,
+}> = (): ReactElement => {
+    return (
+        <Row>
+
+        </Row>
+    )
 }
