4f6699dcdc212afd0e873a011d8c9e69c7b50f73
saniyar.dev <saniyar.dev@gmail.com>
2022-09-06 15:46:11 +0000

    Resolve "form component"

--- src/atomComponents/Grid/Container.tsx
+++ src/atomComponents/Grid/Container.tsx
@@ -4,13 +4,13 @@ import {CSSProperties} from "react";
 type Style = Pick<CSSProperties, "gap" | "width" | "height" | "gridTemplateColumns" | "gridTemplateRows">
 
 const Container = styled.div<{
-    styles: Style
+    styles?: Style
 }>`
     display: grid;
-    width: ${props => props.styles.width};
-    height: ${props => props.styles.height};
-    gap: ${props => props.styles.gap};
-    grid-template-columns: ${props => props.styles.gridTemplateColumns};
-    grid-template-rows: ${props => props.styles.gridTemplateRows};
+    width: ${props => props.styles?.width};
+    height: ${props => props.styles?.height};
+    gap: ${props => props.styles?.gap};
+    grid-template-columns: ${props => props.styles?.gridTemplateColumns};
+    grid-template-rows: ${props => props.styles?.gridTemplateRows};
 `
 export default Container
