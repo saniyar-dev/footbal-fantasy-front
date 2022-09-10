70c57ecf1f426ee779cc31e9d0366c72c28b6ccb
saniyar-dev <saniyar.dev@gmail.com>
2022-09-11 01:35:01 +0430

    fix: list view and global userplayers state

--- src/helpers/useSetAppState.ts
+++ src/helpers/useSetAppState.ts
@@ -1,6 +1,6 @@
 import myPlayers from "@src/state/players";
 import myWallet from "@src/state/wallet";
-import { PLAYER } from "@src/types";
+import { PLAYER, USERPLAYER } from "@src/types";
 import React from "react";
 import { useRecoilState } from "recoil";
 import HTTP from "./axios";
@@ -19,7 +19,28 @@ const useSetAppState = (): {
     // const response = await HTTP.get("user/get-players");
     // setPlayers(response.data as Array<PLAYER>);
 
-    setPlayers([]);
+    const userPlayerList = [...Array(15)]
+      .map((_, index) => index + 1)
+      .reduce(
+        (prev: Array<USERPLAYER>, curr: number): Array<USERPLAYER> => [
+          ...prev,
+          {
+            player_id: -1,
+            first_name: "none",
+            last_name: "none",
+            web_name: "none",
+            photo: "",
+            position:
+              curr <= 2 ? "GK" : curr <= 7 ? "DEF" : curr <= 12 ? "MID" : "ATT",
+            price: 0,
+            score: 0,
+            team_id: 0,
+            squad_place: curr,
+          },
+        ],
+        [] as Array<USERPLAYER>
+      );
+    setPlayers(userPlayerList);
   };
 
   const setPlayerWallet = async () => {
