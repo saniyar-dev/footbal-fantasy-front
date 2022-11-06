3c5c43cfc53920c83500e214a69697eefb4a4de4
saniyar.dev <saniyar.dev@gmail.com>
2022-11-06 08:49:20 +0000

    Resolve "Edit Profile view"

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,38 +1,107 @@
-import React, {FC, ReactElement} from "react";
-import { Form, FormHeader, FormInput, FormPrimaryButton, FormSecondaryButton } from "@src/atomComponents/Form/Form";
+import React, { FC, ReactElement } from "react";
+import {
+  Form,
+  FormHeader,
+  FormInput,
+  FormPrimaryButton,
+  FormSecondaryButton,
+} from "@src/atomComponents/Form/Form";
 import Column from "@src/atomComponents/Grid/Column";
 import Row from "@src/atomComponents/Grid/Row";
 import useAuth from "@src/services/useAuth";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import Button from "@src/atomComponents/Button/Button";
 
-const LoginFormComponent: FC = () : ReactElement => {
-    const {Login} = useAuth()
-    const navigate = useNavigate()
+const PrimaryButton = styled(Button)`
+  background: linear-gradient(90deg, #cf31b9 0%, #9b3af9 73.44%);
+  border-radius: 8px;
+  height: 48px;
+  width: 100%;
 
-    return(
-    <Form onSubmitFn={(data) => Login(data)} styles={{
-        gridTemplateColumns: 'auto',
-        gridTemplateRows: 'auto auto auto',
-        gap: '56px',
-        width: '571px',
-        height: 'fit-content',
-    }}>
-        <FormHeader>
-            ورود به فانتزی
-        </FormHeader>
-        <Column styles={{
-            gap: '16px'
-        }}>
-            <FormInput id="username" label="نام کاربری" placeHolder="asghar" errMessage="از گوگل ترنسلیت استفاده کن و اینو خارجکی بده" />
-            <FormInput id="password" label="رمز عبور" placeHolder="" errMessage="رمز عبورت باید حداقل ۸ حرف باشه و توش یه عددم داشته باشه" />
-        </Column>
-        <Row styles={{
-            gap: '24px'
-        }}>
-            <FormPrimaryButton>ورود</FormPrimaryButton>
-            <FormSecondaryButton onClickFn={() => {navigate("/user/signup")}}>ثبت نام</FormSecondaryButton>
-        </Row>
+  font-weight: 700;
+  font-size: 20px;
+
+  color: #ffffff;
+`;
+
+const SecondaryButton = styled(Button)`
+  box-sizing: border-box;
+  height: 48px;
+  width: 100%;
+
+  background: linear-gradient(#3d185b, #3d185b) padding-box,
+    linear-gradient(90deg, #c847af 1.65%, #9b3af9 43.98%) border-box;
+  border-radius: 8px;
+  border: 2px solid transparent;
+
+  font-weight: 300;
+  font-size: 20px;
+
+  color: #ffffff;
+`;
+
+const LoginFormComponent: FC = (): ReactElement => {
+  const { Login } = useAuth();
+  const navigate = useNavigate();
+
+  return (
+    <Form
+      onSubmitFn={(data) => Login(data)}
+      styles={{
+        gridTemplateColumns: "auto",
+        gridTemplateRows: "auto auto auto",
+        gap: "56px",
+        width: "571px",
+        height: "fit-content",
+      }}
+    >
+      <FormHeader>ورود به فانتزی</FormHeader>
+      <Column
+        styles={{
+          gap: "16px",
+        }}
+      >
+        <FormInput
+          styles={{
+            background: "#3D185B",
+            color: "#EDD8FF",
+            border: "1px solid #A057DB",
+          }}
+          id="username"
+          label="نام کاربری"
+          placeHolder="asghar"
+          errMessage="از گوگل ترنسلیت استفاده کن و اینو خارجکی بده"
+        />
+        <FormInput
+          styles={{
+            background: "#3D185B",
+            color: "#EDD8FF",
+            border: "1px solid #A057DB",
+          }}
+          id="password"
+          label="رمز عبور"
+          placeHolder=""
+          errMessage="رمز عبورت باید حداقل ۸ حرف باشه و توش یه عددم داشته باشه"
+        />
+      </Column>
+      <Row
+        styles={{
+          gap: "24px",
+        }}
+      >
+        <FormPrimaryButton>
+          <PrimaryButton>ورود</PrimaryButton>
+        </FormPrimaryButton>
+        <FormSecondaryButton
+          onClickFn={() => {
+            navigate("/user/signup");
+          }}
+        >
+          <SecondaryButton>ثبت نام</SecondaryButton>
+        </FormSecondaryButton>
+      </Row>
     </Form>
-    )
-}
-export default LoginFormComponent 
+  );
+};
+export default LoginFormComponent;
--- src/components/SignUpForm/SignUpForm.tsx
+++ src/components/SignUpForm/SignUpForm.tsx
@@ -1,37 +1,118 @@
-import React, {FC, ReactElement} from "react";
-import { Form, FormHeader, FormInput, FormPrimaryButton } from "@src/atomComponents/Form/Form";
+import React, { FC, ReactElement } from "react";
+import {
+  Form,
+  FormHeader,
+  FormInput,
+  FormPrimaryButton,
+} from "@src/atomComponents/Form/Form";
 import Container from "@src/atomComponents/Grid/Container";
 import useAuth from "@src/services/useAuth";
+import styled from "styled-components";
+import Button from "@src/atomComponents/Button/Button";
 
+const PrimaryButton = styled(Button)`
+  background: linear-gradient(90deg, #cf31b9 0%, #9b3af9 73.44%);
+  border-radius: 8px;
+  height: 48px;
+  width: 100%;
 
-const SignupFormComponent: FC = () : ReactElement => {
-    const {Signup} = useAuth()
-    return(
-        <Form onSubmitFn={(data) => Signup(data)} styles={{
-            gridTemplateColumns: 'auto',
-            gridTemplateRows: 'auto auto auto',
-            gap: '48px',
-            width: '571px',
-            height: 'fit-content',
-        }}>
-            <FormHeader>
-                فرم ثبت نام
-            </FormHeader>
-            <Container styles={{
-                gridTemplateColumns: 'auto auto',
-                gridTemplateRows: 'auto auto auto',
-                gap: '24px',
-            }}>
+  font-weight: 700;
+  font-size: 20px;
 
-                <FormInput id="name" label="نام" placeHolder="مثلا محمود" errMessage="اینجا ایرانه ولی تو اسمتو خارجکی تایپ کن" />
-                <FormInput id="lastname" label="نام خانوادگی" placeHolder="مثلا محمودی" errMessage="اینم خارجکی لطف کن" />
-                <FormInput id="email" label="ایمیل" placeHolder="mahmood@gmail.com" errMessage="این قراره کار کنه یه واقعیشو بده" />
-                <FormInput id="country" label="کشور" placeHolder="قم" />
-                <FormInput id="username" label="نام کاربری" placeHolder="mahmoodMahmoodi" errMessage="اینجا همه چی خارجکیه" />
-                <FormInput id="password" label="رمز عبور" placeHolder="" errMessage="رمز عبورت باید حداقل ۸ حرف و یه عدد توش باشه و البته که خارجکی" />
-            </Container>
-            <FormPrimaryButton >ثبت نام</FormPrimaryButton>
-        </Form>
-    )
-}
-export default SignupFormComponent
+  color: #ffffff;
+`;
+
+const SignupFormComponent: FC = (): ReactElement => {
+  const { Signup } = useAuth();
+  return (
+    <Form
+      onSubmitFn={(data) => Signup(data)}
+      styles={{
+        gridTemplateColumns: "auto",
+        gridTemplateRows: "auto auto auto",
+        gap: "48px",
+        width: "571px",
+        height: "fit-content",
+      }}
+    >
+      <FormHeader>فرم ثبت نام</FormHeader>
+      <Container
+        styles={{
+          gridTemplateColumns: "auto auto",
+          gridTemplateRows: "auto auto auto",
+          gap: "24px",
+        }}
+      >
+        <FormInput
+          styles={{
+            background: "#3D185B",
+            color: "#EDD8FF",
+            border: "1px solid #A057DB",
+          }}
+          id="name"
+          label="نام"
+          placeHolder="مثلا محمود"
+          errMessage="اینجا ایرانه ولی تو اسمتو خارجکی تایپ کن"
+        />
+        <FormInput
+          styles={{
+            background: "#3D185B",
+            color: "#EDD8FF",
+            border: "1px solid #A057DB",
+          }}
+          id="lastname"
+          label="نام خانوادگی"
+          placeHolder="مثلا محمودی"
+          errMessage="اینم خارجکی لطف کن"
+        />
+        <FormInput
+          styles={{
+            background: "#3D185B",
+            color: "#EDD8FF",
+            border: "1px solid #A057DB",
+          }}
+          id="email"
+          label="ایمیل"
+          placeHolder="mahmood@gmail.com"
+          errMessage="این قراره کار کنه یه واقعیشو بده"
+        />
+        <FormInput
+          styles={{
+            background: "#3D185B",
+            color: "#EDD8FF",
+            border: "1px solid #A057DB",
+          }}
+          id="country"
+          label="کشور"
+          placeHolder="قم"
+        />
+        <FormInput
+          styles={{
+            background: "#3D185B",
+            color: "#EDD8FF",
+            border: "1px solid #A057DB",
+          }}
+          id="username"
+          label="نام کاربری"
+          placeHolder="mahmoodMahmoodi"
+          errMessage="اینجا همه چی خارجکیه"
+        />
+        <FormInput
+          styles={{
+            background: "#3D185B",
+            color: "#EDD8FF",
+            border: "1px solid #A057DB",
+          }}
+          id="password"
+          label="رمز عبور"
+          placeHolder=""
+          errMessage="رمز عبورت باید حداقل ۸ حرف و یه عدد توش باشه و البته که خارجکی"
+        />
+      </Container>
+      <FormPrimaryButton>
+        <PrimaryButton>ثبت نام</PrimaryButton>
+      </FormPrimaryButton>
+    </Form>
+  );
+};
+export default SignupFormComponent;
