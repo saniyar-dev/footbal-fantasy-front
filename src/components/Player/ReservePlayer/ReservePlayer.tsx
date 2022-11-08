f19b43e9c27a951cb013b5093019d1021cb65474
saniyar <saniyar.dev@gmail.com>
2022-11-08 05:34:05 +0330

    fix: bench UI

--- src/components/Player/ReservePlayer/ReservePlayer.tsx
+++ src/components/Player/ReservePlayer/ReservePlayer.tsx
@@ -1,92 +1,98 @@
 import Column from "@src/atomComponents/Grid/Column";
 import { usePlayerLogic } from "@src/atomComponents/Player/Player";
 import { USERPLAYER } from "@src/types";
-import React, {FC, ReactElement} from "react";
+import React, { FC, ReactElement } from "react";
 import styled from "styled-components";
 import Row from "@src/atomComponents/Grid/Row";
 import useModal from "@src/helpers/useModal";
 import { useRecoilState } from "recoil";
 import { _selectedPlayer } from "@src/state/players";
 
-import ActivePlayerUrl from '@assets/Images/Player/ActivePlayer.svg'
+import ActivePlayerUrl from "@assets/Images/Player/ActivePlayer.svg";
 
-const ActiveImg = () => <img src={ActivePlayerUrl} alt="active player" />
+const ActiveImg = () => <img src={ActivePlayerUrl} alt="active player" />;
 
 const CustomPlayerColumn = styled(Column)`
   z-index: 2;
-`
+`;
 
 const ActivePlayerDetail = styled(Row)`
-background: #37013B;
-border-radius: 4px;
-justify-content: center;
-align-items: center;
+  background: #37013b;
+  border-radius: 4px;
+  justify-content: center;
+  align-items: center;
 
-width: 120px;
-height: 25px;
+  width: 120px;
+  height: 25px;
 
-font-family: 'Vazirmatn';
-font-style: normal;
-font-weight: 700;
-font-size: 13px;
-line-height: 12px;
-text-align: center;
+  font-family: "Vazirmatn";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 13px;
+  line-height: 12px;
+  text-align: center;
 
-color: #FFFFFF;
-`
+  color: #ffffff;
+`;
 
 const SelectedPlayerDetail = styled(Row)`
-background: #05F4F1;
-border-radius: 4px;
-justify-content: center;
-align-items: center;
+  background: #05f4f1;
+  border-radius: 4px;
+  justify-content: center;
+  align-items: center;
 
-width: 120px;
-height: 25px;
+  width: 120px;
+  height: 25px;
 
-font-family: 'Vazirmatn';
-font-style: normal;
-font-weight: 700;
-font-size: 13px;
-line-height: 12px;
-text-align: center;
+  font-family: "Vazirmatn";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 13px;
+  line-height: 12px;
+  text-align: center;
 
-color: #FFFFFF;
-`
+  color: #ffffff;
+`;
 
 const ReservePlayer: FC<{
-    playerInfo: USERPLAYER
-}> = ({playerInfo}): ReactElement => {
-    const {status, setToSelected} = usePlayerLogic({playerInfo, local: false, clickActive: true})
-    const {addModal} = useModal()
-    const [selectedPlayer, ] = useRecoilState(_selectedPlayer)
+  playerInfo: USERPLAYER;
+}> = ({ playerInfo }): ReactElement => {
+  const { status, setToSelected } = usePlayerLogic({
+    playerInfo,
+    local: false,
+    clickActive: true,
+  });
+  const { addModal } = useModal();
+  const [selectedPlayer] = useRecoilState(_selectedPlayer);
 
-    return <CustomPlayerColumn>
-    {
-            status === 'Active' ? <Column onClick={() => {
-                if (selectedPlayer) {
-                    addModal({
-                        _tag: 'change-player',
-                        playerIn: playerInfo,
-                        playerOut: selectedPlayer
-                    })
-                } else {
-                    setToSelected(playerInfo)
-                }
-            }}>
-                <ActiveImg />
-                <ActivePlayerDetail>{playerInfo.web_name}</ActivePlayerDetail>
-            </ Column> : undefined
-    }
-    {
-        status === 'Selected' ? <Column onClick={() => setToSelected("none")}>
-            <ActiveImg />
-            <SelectedPlayerDetail>
-                {playerInfo.web_name}
-            </SelectedPlayerDetail>
-        </Column> : undefined
-    }
+  return (
+    <CustomPlayerColumn>
+      {status === "Active" ? (
+        <Column
+          onClick={() => {
+            if (selectedPlayer) {
+              addModal({
+                _tag: "change-player",
+                playerIn: playerInfo,
+                playerOut: selectedPlayer,
+              });
+            } else {
+              setToSelected(playerInfo);
+            }
+          }}
+        >
+          <ActiveImg />
+          <ActivePlayerDetail>{playerInfo.web_name}</ActivePlayerDetail>
+        </Column>
+      ) : undefined}
+      {status === "Selected" ? (
+        <Column onClick={() => setToSelected("none")}>
+          <ActiveImg />
+          <SelectedPlayerDetail>{playerInfo.web_name}</SelectedPlayerDetail>
+        </Column>
+      ) : undefined}
     </CustomPlayerColumn>
-}
+  );
+};
 
-export default ReservePlayer
+export default ReservePlayer;
--- src/components/SubtituteField/SubtituteField.tsx
+++ src/components/SubtituteField/SubtituteField.tsx
@@ -2,39 +2,39 @@ import Column from "@src/atomComponents/Grid/Column";
 import { PlayersContainer } from "@src/atomComponents/Player/Player";
 import SectionHeader from "@src/atomComponents/SectionHeader/SectionHeader";
 import useAppState from "@src/services/useAppState";
-import React, {FC, ReactElement} from "react";
+import React, { FC, ReactElement } from "react";
 import styled from "styled-components";
 import ReservePlayer from "../Player/ReservePlayer/ReservePlayer";
 
 const StyledColumn = styled(Column)`
-width: 100%;
-height: 100%;
-filter: drop-shadow(0px 0px 10px rgba(0, 0, 0, 0.12));
-background: #FFFFFF;
-border-radius: 0 0 16px 16px;
-`
+  width: 100%;
+  height: 100%;
+  filter: drop-shadow(0px 0px 10px rgba(0, 0, 0, 0.12));
+  background: #ffffff;
+  border-radius: 0 0 16px 16px;
+`;
 
 const SubtituteField: FC = (): ReactElement => {
-    const {reservePlayers} = useAppState()
-    return <StyledColumn>
-        <SectionHeader>
-            بازیکنان ذخیره
-        </SectionHeader>
-        <PlayersContainer>
-            <Column styles={{
-                gap: '24px',
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: '100%'
-            }}>
-                {
-                    reservePlayers.map((player, index) => {
-                        return <ReservePlayer playerInfo={player} key={index} />
-                    })
-                }
-            </Column>
-        </PlayersContainer>
+  const { reservePlayers } = useAppState();
+  return (
+    <StyledColumn>
+      <SectionHeader>بازیکنان ذخیره</SectionHeader>
+      <PlayersContainer>
+        <Column
+          styles={{
+            gap: "10px",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100%",
+          }}
+        >
+          {reservePlayers.map((player, index) => {
+            return <ReservePlayer playerInfo={player} key={index} />;
+          })}
+        </Column>
+      </PlayersContainer>
     </StyledColumn>
-}
+  );
+};
 
-export default SubtituteField
+export default SubtituteField;
