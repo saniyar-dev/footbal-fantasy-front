dbe2a7d168ec6d2a6840318effa45a7f05e8cbf2
saniyar <saniyar.dev@gmail.com>
2022-11-08 06:48:18 +0330

    add: useProfile hook

---
+++ src/services/useProfile.ts
@@ -0,0 +1,63 @@
+import useToast from "@src/helpers/useToast";
+import { User } from "@src/types";
+import { useEffect, useState } from "react";
+
+type GodInfoType = Omit<User, "isFollowing">;
+
+const useProfile = (): {
+  godInfo: GodInfoType;
+} => {
+  const { addToast } = useToast();
+  const [godInfo, setGodInfo] = useState<GodInfoType>({
+    name: "asghar",
+    userId: "",
+    country: "ایران",
+    profilePic: undefined,
+    username: "asghar",
+  });
+
+  const getGodInfo = async (): Promise<GodInfoType> => {
+    try {
+      return {
+        name: "asghar",
+        userId: "",
+        country: "ایران",
+        profilePic: undefined,
+        username: "asghar",
+      };
+    } catch (err) {
+      console.log(err);
+      addToast({
+        _tag: "error",
+        message: "یه چیزی رفت رو هوا صفحه رو رفرش کن",
+      });
+      return {
+        name: "asghar",
+        userId: "",
+        country: "ایران",
+        profilePic: undefined,
+        username: "asghar",
+      };
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setGodInfo(await getGodInfo());
+    };
+
+    fetchData().catch((err) => {
+      console.log(err);
+      addToast({
+        _tag: "error",
+        message: "یه چیزی رفت رو هوا صفحه رو رفرش کن",
+      });
+    });
+  }, []);
+
+  return {
+    godInfo,
+  };
+};
+
+export default useProfile;
